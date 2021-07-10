46494ad7cbaeb63fe59f70ea5d47324c91f69d02
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-07-10 14:40:17 -0400

    set up basic layout

--- game_screen.py
+++ game_screen.py
@@ -5,13 +5,23 @@ import urwid
 
 def render_game_screen() -> None:
     """Display the main game screen"""
-    text = urwid.Text("Welcome, this is the game screen...")
+    text = urwid.Text("placeholder")
     fill = urwid.Filler(text, 'top')
 
-    box = urwid.LineBox(fill, title="Game Screen")
+    # The main boxes
+    location_box = urwid.LineBox(fill, title="location")
+    stats_box = urwid.LineBox(fill, title="stats")
+    event_box = urwid.LineBox(fill, title="event")
+
+    # Arrange a pile with two columns on top and events on bottom
+    top_columns = urwid.Columns([location_box, stats_box])
+    pile = urwid.Pile([top_columns, event_box])
+
+    # Put everything on one box
+    box = urwid.LineBox(pile, title="Game Screen")
 
     def handle_input(key: str) -> None:
-        if key.lower() == 'q':
+        if str(key).lower() == 'q':
             loop.stop()
             quit()
 
