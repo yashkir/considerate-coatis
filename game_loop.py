610780547462ea0dc9d71e3e438beb9758a351d8
Icode-python <me.fast.learn@gmail.com>
2021-07-10 19:43:16 -0400

    got the basic TUI setup with curses

--- game_loop.py
+++ game_loop.py
@@ -1,4 +1,5 @@
 import curses
+from widgets import *
 
 class game:
 
@@ -8,11 +9,15 @@ class game:
         self.screen = curses.initscr()
         curses.noecho()
         curses.cbreak()
+        curses.curs_set(False)
         self.screen.keypad(True)
+        self.height = self.screen.getmaxyx()[0]-1
+        self.width = self.screen.getmaxyx()[1]-1
 
     def update(self):
+        
         ...
-    
+
     def run(self):
         self.playing = True
         while self.playing:
@@ -23,12 +28,18 @@ class game:
     
     def handle_input(self):
         key = self.screen.getkey()
-        print(key)
-        if key == "K_ESCAPE" or key == "q":
+        #print(key)
+        if key == "ESCAPE" or key == "q":
             curses.endwin()
             self.playing = False
     
     def render_game_screen(self) -> None:
+        self.height = self.screen.getmaxyx()[0]-1
+        self.width = self.screen.getmaxyx()[1]-1
+        draw_square(self.screen, 0, 0, self.width, self.height, "game")
+        draw_square(self.screen, 2, 1, round(self.width-self.width/3-2), round(self.height-self.height/3-1), "screen")
+        draw_square(self.screen, round(self.width-self.width/3)+1, 1, round(self.width/3)-3, round(self.height-self.height/3-1), "stats")
+        draw_square(self.screen, 2, round(self.height-self.height/3), self.width-4, round(self.height/3-1), "Text")
         ...
 
 g = game()
---
+++ widgets.py
@@ -0,0 +1,26 @@
+import curses
+#`│ ─ ┌ ┬ ┐ ├ ┼ ┤ └ ┴ ┘`
+
+def draw_square(screen, x, y, width, height, label):
+    for wx in range(width):
+        for hy in range(height):
+            if wx == 0 and hy == 0:
+                screen.addstr(y+hy, x+wx, "┌")
+            elif wx == width-1 and hy == height-1:
+                screen.addstr(y+hy, x+wx, "┘")
+            elif wx == 0 and hy == height-1:
+                screen.addstr(y+hy, x+wx, "└")
+            elif wx == width-1 and hy == 0:
+                screen.addstr(y+hy, x+wx, "┐")
+            elif hy == 0:
+                 screen.addstr(y+hy, x+wx, "─")
+            elif wx == 0:
+                screen.addstr(y+hy, x+wx, "│")
+            elif hy == height-1:
+                screen.addstr(y+hy, x+wx, "─")
+            elif wx == width-1:
+                screen.addstr(y+hy, x+wx, "│")
+    
+    width -= len(label)
+    screen.addstr(y, round(width/2+x), label)
+    ...
