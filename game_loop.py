4eb9a9b0a3fc5cc6e270c08690ddbc2f2f38800c
Icode-python <me.fast.learn@gmail.com>
2021-07-10 15:53:08 -0400

    starting mainloop

--- game_loop.py
+++ game_loop.py
@@ -1,1 +1,47 @@
-# test
+import urwid
+
+class game:
+
+    def __init__(self) -> None:
+        self.playing = False
+        self.widgets = []
+        #self.loop = urwid.MainLoop(self.widgets, unhandled_input=self.handle_input)
+
+    def update(self):
+        ...
+    
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.render_game_screen()
+            self.update()
+            self.handle_input()
+    
+    def handle_input(self, key: str):
+        if str(key).lower() == 'q':
+            self.loop.stop()
+            quit()
+    
+    def render_game_screen(self) -> None:
+        """Display the main game screen"""
+        text = urwid.Text("placeholder")
+        fill = urwid.Filler(text, 'top')
+
+        # The main boxes
+        location_box = urwid.LineBox(fill, title="location")
+        stats_box = urwid.LineBox(fill, title="stats")
+        event_box = urwid.LineBox(fill, title="event")
+
+        # Arrange a pile with two columns on top and events on bottom
+        top_columns = urwid.Columns([location_box, stats_box])
+        pile = urwid.Pile([top_columns, event_box])
+
+        # Put everything on one box
+        box = urwid.LineBox(pile, title="Game Screen")
+
+        self.loop = urwid.MainLoop(box, unhandled_input=self.handle_input)
+
+        self.loop.run()
+
+g = game()
+g.run()
