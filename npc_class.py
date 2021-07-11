9831cd7c380ce0ba846f5012e96da453870253e2
Icode-python <me.fast.learn@gmail.com>
2021-07-10 22:12:04 -0400

    added base npc class

--- npc_class.py
+++ npc_class.py
@@ -1,11 +1,18 @@
 
 class npc:
 
-    def __init__(self, charisma, strength, wisdom, smartness):
+    def __init__(self, charisma, strength, wisdom, smartness, x, y, game):
         self.charisma = charisma
         self.strength = strength
         self.wisdom = wisdom
         self.smartness = smartness
+        self.x = x
+        self.y = y
+        self.game = game
+        self.screen = self.game.screen
     
-    
+    def say_prompt(self, x, y):
+        prompt = " "
+        # prompt = get_prompt()
+        self.game.screen.addstr(y, x, prompt)
         
