c32a679eb1bc68cddb9b7e116bfcac72fccb457e
Icode-python <me.fast.learn@gmail.com>
2021-07-11 17:26:34 -0400

    trying to fix linting issues

--- game_classes/npc_class.py
+++ game_classes/npc_class.py
@@ -1,6 +1,7 @@
 # from player import Stats
 
 class npc:
+    """Npc class"""
 
     def __init__(self, stats, x, y, game):
         self.stats = stats
@@ -8,10 +9,9 @@ class npc:
         self.y = y
         self.game = game
         self.screen = self.game.screen
-    
+
     def say_prompt(self, x, y):
+        """What the npc says"""
         prompt = " "
         # prompt = get_prompt()
         self.game.screen.addstr(y, x, prompt)
-
-        
--- game_classes/player.py
+++ game_classes/player.py
@@ -1,28 +1,28 @@
 
 class Stats:
+    """Stats class to store all the stats that the npc and player will have."""
 
     def __init__(self, athletic_ablity, charisma, wisdom, smartness):
         self.atletic_ability = athletic_ablity
         self.charisma = charisma
         self.wisdom = wisdom
         self.smartness = smartness
-        self.stat_list = ["athletic ability: " + str(self.atletic_ability), "charisma: " + str(self.charisma), "wisdom: " + str(self.wisdom), "smartness: " + str(self.smartness)]
+        self.stat_list = [
+            "athletic ability: " + str(self.atletic_ability), "charisma: " + str(self.charisma),
+            "wisdom: " + str(self.wisdom), "smartness: " + str(self.smartness)]
 
     def update_stats(self):
-        self.stat_list = ["athletic ability: " + str(self.atletic_ability), "charisma: " + str(self.charisma), "wisdom: " + str(self.wisdom), "smartness: " + str(self.smartness)]
+        """This will update the stats"""
+        self.stat_list = [
+            "athletic ability: " + str(self.atletic_ability), "charisma: " + str(self.charisma),
+            "wisdom: " + str(self.wisdom), "smartness: " + str(self.smartness)]
+
 
 class Player:
+    """The player class"""
 
     def __init__(self, stats, x, y, game):
         self.stats = stats
         self.x = x
         self.y = y
         self.game = game
-    
-    def draw_stats(self):
-        self.stats.update_stats()
-        screen = self.game.screen
-        for y in range(len(self.stats.stat_list)):
-            screen.addstr(self.game.stats_box_pos[0]+y*2+1, self.game.stats_box_pos[1]+1, str(self.stats.stat_list[y]))
-            ...
-        
