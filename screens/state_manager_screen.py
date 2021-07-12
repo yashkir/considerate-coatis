837f618e8eec15347d9ef765ac26c73ed4d840e5
Icode-python <me.fast.learn@gmail.com>
2021-07-12 12:38:45 -0400

    TUI can show all save options

--- screens/state_manager_screen.py
+++ screens/state_manager_screen.py
@@ -14,8 +14,10 @@ class StateManagerScreen(urwid.LineBox):
         for path in pathlib.Path("saves").iterdir():
             if path.is_file():
                 saves.append(path)
-                save_buttons.append(urwid.filler(urwid.Text(str.split(path.name, '.')[0]), 'center'), 'middle')
-        buttons = urwid.Filler(urwid.GridFlow([button_no], 10, 5, 1, 'center'))
+                save_buttons.append(urwid.Button(str.split(path.name, '.')[0], self.__load_save))
+
+        buttons_grid = urwid.GridFlow([button_no]+save_buttons, 10, 5, 1, 'center')
+        buttons = urwid.Filler(buttons_grid)
 
         super().__init__(urwid.Pile([text, buttons]), title="load save")
 
