7f5bdf49221e9ffb380137bde0b5668507ce2f8e
Icode-python <me.fast.learn@gmail.com>
2021-07-12 20:38:35 -0400

    fixed problem where couldn't see cursor becuase using list walker, switched to line box and works

--- logic/player.py
+++ logic/player.py
@@ -10,10 +10,19 @@ class Stats:
         self.wisdom = wisdom
         self.smartness = smartness
         self.stat_list = [self.athletic_ability, self.charisma, self.wisdom, self.smartness]
-        self.stat_list_text = [
-            urwid.Text("strength: " + str(self.athletic_ability)),
-            urwid.Text("charisma: " + str(self.charisma)),
-            urwid.Text("wisdom: " + str(self.wisdom)), urwid.Text("smartness: " + str(self.smartness))]
+        self.stat_list_text = urwid.Filler(urwid.Text(
+            "strength: " + str(self.athletic_ability)+'\n'
+            "charisma: " + str(self.charisma)+'\n'
+            "wisdom: " + str(self.wisdom) + '\n'
+            "smartness: " + str(self.smartness) + '\n',), 'middle')
+
+    def update_stats(self):
+        """This is where the stats will be updated"""
+        self.stat_list_text = urwid.Filler(urwid.Text(
+            "strength: " + str(self.athletic_ability)+'\n'
+            "charisma: " + str(self.charisma)+'\n'
+            "wisdom: " + str(self.wisdom) + '\n'
+            "smartness: " + str(self.smartness) + '\n',), 'middle')
 
 
 class Player:
@@ -24,11 +33,3 @@ class Player:
         self.x = x
         self.y = y
         # self.stats_fill = urwid.Filler(self.stats.stat_list_text, 'top')
-
-    def update_stats(self):
-        """This is where the player's stats will be updated"""
-        self.stats.stat_list_text = [
-            urwid.Text("strength: " + str(self.stats.athletic_ability)),
-            urwid.Text("charisma: " + str(self.stats.charisma)),
-            urwid.Text("wisdom: " + str(self.stats.wisdom)), urwid.Text("smartness: " + str(self.stats.smartness))]
-        ...
--- screens/game_screen.py
+++ screens/game_screen.py
@@ -9,14 +9,32 @@ class GameScreen(urwid.LineBox):
         self.fill = urwid.Filler(self.text, 'top')
         self.player = player
 
-        self.update()
+        # The main boxes
+        self.stats_box = urwid.LineBox(self.player.stats.stat_list_text, title="stats")
+        self.location_box = urwid.LineBox(self.fill, title="location")
+        self.event_box = urwid.LineBox(self.fill, title="event")
 
-    def update(self):
+        # buttons
+        self.button_one = urwid.Button("quit", lambda _: self._emit('quit'))
+        self.button_two = urwid.Button("Restart", lambda _: self._emit('restart'))
+        self.button_columns = urwid.Columns([
+            urwid.Filler(self.button_one, 'top'), urwid.Filler(self.button_two, 'top')])
+
+        self.button_box = urwid.LineBox(self.button_columns, title="buttons")
+
+        # Arrange a pile with two columns on top and events on bottom
+        self.top_columns = urwid.Columns([('weight', 3.5, self.location_box), self.stats_box])
+        self.pile = urwid.Pile([('weight', 3, self.top_columns), ('weight', 1.5, self.event_box), self.button_box])
+
+        # Put everything on one box
+        # box = urwid.LineBox(pile, title="Game Screen")
+        super().__init__(self.pile, title="Game Screen")
+        # self.update()
+
+    def __update(self):
         """The update variable to update everything on the screen at will"""
         # The main boxes
-        self.stats_box = urwid.LineBox(
-            urwid.ListBox(urwid.SimpleFocusListWalker(self.player.stats.stat_list_text)),
-            title="stats")
+        self.stats_box = urwid.LineBox(self.player.stats.stat_list_text, title="stats")
         self.location_box = urwid.LineBox(self.fill, title="location")
         self.event_box = urwid.LineBox(self.fill, title="event")
 
@@ -36,16 +54,14 @@ class GameScreen(urwid.LineBox):
         # box = urwid.LineBox(pile, title="Game Screen")
         super().__init__(self.pile, title="Game Screen")
 
-        ...
-
     def keypress(self, size, key):
         """Handle q for quitting"""
         key = super().keypress(size, key)
         if str(key).lower() == 'r':
             self.text.set_text("random")
             self.player.stats.charisma += 1
-            self.player.update_stats()
-            self.update()
+            self.player.stats.update_stats()
+            self.__update()
         if str(key).lower() == 'q':
             raise urwid.ExitMainLoop()
 
