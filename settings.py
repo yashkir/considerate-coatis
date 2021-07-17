b7e029c1e5b83ddbd5ba5a3cdec85332d9070685
Yuriy Yashkir <yuriy.yashkir@gmail.com>
2021-07-16 22:23:27 -0400

    add 'faded' color

--- settings.py
+++ settings.py
@@ -4,5 +4,6 @@ OVERLAY_HEIGHT = 40
 PALETTE_COLORS = 256
 PALETTE = [
     ('highlight', '', '', '', '#ffa', 'black'),
+    ('faded', '', '', '', '#888', 'black'),
     ('background', '', '', '', '#00f', '#006'),
 ]
